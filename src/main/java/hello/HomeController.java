a7639729ea6b4801a44b0170a4e590d2d57ea95c
kayartaya-vinod <vinod@vinod.co>
2019-09-08 19:21:17 +0530

    🇮🇳

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -1,15 +1,27 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
     @GetMapping("/")
-    public @ResponseBody String greeting() {
+    public String greeting() {
         return "Hello World";
+    }
+    
+    @GetMapping("/{name}")
+    public Map<String, String> greeting(@PathVariable String name) {
+    	Map<String, String> m = new HashMap<String, String>();
+    	m.put("message", String.format("Hello, %s!", name));
+    	m.put("time", new Date().toString());
+    	return m;
     }
 
 }
