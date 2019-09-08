c7f0dfb53d6f3cb74d84e423b3a78dadb246fe0c
kayartaya-vinod <vinod@vinod.co>
2019-09-08 19:49:02 +0530

    🇮🇳

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -1,11 +1,6 @@
 package hello;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,14 +11,6 @@ public class HomeController {
 		return "Hello World";
 	}
 
-	@GetMapping("/{name}")
-	public Map<String, String> greeting(@PathVariable String name) {
-		// introduce code smells so that quality drops from 'A' to 'B'
-		// int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
-		Map<String, String> msg = new HashMap<String, String>();
-		msg.put("message", String.format("Hello, %s!", name));
-		msg.put("time", new Date().toString());
-		return msg;
-	}
+	
 
 }
